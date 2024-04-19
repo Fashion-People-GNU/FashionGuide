30ceec747d44fdab2eedff9a54a0a55210d42b18
차도훈 <cha_dh1004@naver.com>
2024-04-19 20:02:07 +0900

    [FIX] 바텀 네비게이션 이름 수정

--- app/src/main/java/com/fashionPeople/fashionGuide/data/Screen.kt
+++ app/src/main/java/com/fashionPeople/fashionGuide/data/Screen.kt
@@ -10,8 +10,8 @@ import androidx.compose.ui.graphics.vector.ImageVector
 
 
 sealed class Screen(val route: String, val icon: ImageVector) {
-    data object Home : Screen("home", Icons.Default.Home)
-    data object Closet : Screen("Closet", Icons.Default.List)
-    data object Settings : Screen("settings", Icons.Default.Settings)
+    data object Home : Screen("옷 추천", Icons.Default.Home)
+    data object Closet : Screen("옷장", Icons.Default.List)
+    data object Settings : Screen("설정", Icons.Default.Settings)
 
 }
